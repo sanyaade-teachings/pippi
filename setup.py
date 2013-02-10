5cca3f776b5a931b9258acb291b58fb88945727b
Erik Schoster <erik@heythere.org>
2013-02-10 16:57:23 -0600

    Switch to using distribute/setuptools for install. Closes #5

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
+import distribute_setup
+distribute_setup.use_setuptools()
+
 from setuptools import setup
 
 setup(name='pippi',
-        version='0.1.1',
-        description='A command driven computer music instrument',
+        version='0.1.1-1',
+        description='A python computer music system',
         url='http://hecanjog.github.com/pippi',
         author='He Can Jog',
         author_email='erik@hecanjog.com',
         license='Public Domain',
         packages=['pippi'],
-        install_requires=[
-            'pyalsaaudio',
-            'termcolor',
-            'vcosc',
-        ],
+        extras_require = {
+            'realtime': ['pyalsaaudio', 'termcolor', 'vcosc'],
+        },
         test_suite='nose.collector',
         tests_require=['nose'],
         scripts=['bin/pippi'],
