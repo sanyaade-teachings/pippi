dfb20937bbd66f2b0ea10f34f55772f9654f7f3f
Erik Schoster <erik@heythere.org>
2013-08-06 23:52:45 -0500

    Add pan and envelope example.

--- docs/dsp_chicago.py
+++ docs/dsp_chicago.py
@@ -75,10 +75,36 @@ for i in range(20):
 # Lets pause here and render out what we've got so far.
 # To render all the tones as a single sound file, we've got to 
 # first join our list together into a single sound.
-short_tones_output = ''.join(short_tones)
+output = ''.join(short_tones)
 
 # Now we can use pippi's write() function to render this sound to disk.
 # The second argument is the filename to write to. A .wav extension will
 # automatically be added. (Pippi can only read and write WAV data for now!)
-dsp.write(short_tones_output, 'short_tones')
+dsp.write(output, 'short_tones')
 
+# Next, lets create a simple function to randomly pan each tone to a different 
+# position in the stereo field, and apply a randomly selected amplitude envelope
+# to each tone.
+def pan_and_envelope(short_tones):
+    # A few of the possible envelope types the env() function may use
+    envelope_types = ['hann', 'sine', 'line', 'phasor']
+
+    # Loop through each tone and randomly pan it. pan() takes a sound as its first 
+    # argument and a float between 0 and 1 as the second. 0 is hard left, 1 is hard right.
+    # By default, rand() will return a float between 0 and 1 when no arguments are passed.
+    short_tones = [ dsp.pan(short_tone, dsp.rand()) for short_tone in short_tones ]
+
+    # Loop through each (now panned) tone and apply a randomly selected amplitude envelope
+    # from the above set of possible envelope types.
+    short_tones = [ dsp.env(short_tone, dsp.randchoose(envelope_types)) for short_tone in short_tones ]
+
+    return short_tones
+
+# Take our short_tones and process them, storing the result in a new list: short_tones_pan_envelope
+short_tones_pan_envelope = pan_and_envelope(short_tones)
+
+# Join the new tones so we can write the whole thing to disk. And store it in 'output'
+output = ''.join(short_tones_pan_envelope)
+
+# Write output to disk
+dsp.write(output, 'short_tones_pan_envelope')
