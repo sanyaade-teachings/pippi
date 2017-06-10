2854a53ce90d772d8b3f6ef70e872a87097ea30e
Erik Schoster <ejschoster@gmail.com>
2017-06-09 20:35:27 -0500

    Remove some dead code in rhythm module

--- pippi/rhythm.py
+++ pippi/rhythm.py
@@ -77,22 +77,6 @@ def eu(length, numbeats, offset=0, reps=None, reverse=False):
 
     return pattern
 
-
-def onsets(pattern, beatlength=4410, offset=0, reps=None, reverse=False, playhead=0):
-    """ TODO: convert ascii and last patterns into onset lists
-    """
-    out = []
-    for beat in pattern:
-        if beat in HIT_SYMBOLS:
-            out += [ playhead ]
-
-        playhead += beatlength
-
-    if reverse:
-        out = reversed(out)
-
-    return out
-
 def curve(numbeats=16, wintype=None, length=44100, reverse=False):
     """ Bouncy balls
     """
@@ -105,16 +89,10 @@ def curve(numbeats=16, wintype=None, length=44100, reverse=False):
 
     return [ int(onset * length) for onset in win ]
 
-
 def rotate(pattern, offset=0):
     """ Rotate a pattern list by a given offset
     """
     return pattern[-offset % len(pattern):] + pattern[:-offset % len(pattern)]
-
-def scale(onsets, factor):
-    """ Scale a list of onsets by a given factor
-    """
-    return [ int(onset * factor) for onset in onsets ]
 
 def repeat(onsets, reps):
     """ Repeat a sequence of onsets a given number of times
