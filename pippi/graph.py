dd815c7299ed984237eca4b2a4e9385f9c644b01
Erik Schoster <ejschoster@gmail.com>
2017-07-18 22:17:35 -0500

    Tweak waveform drawing. Waveform drawing is slow.

--- pippi/graph.py
+++ pippi/graph.py
@@ -2,29 +2,28 @@ import random
 from PIL import Image, ImageDraw
 from . import interpolation
 
-def waveform(sound, filename=None, width=400, height=200, stroke=None, upsample_mult=10):
+def waveform(sound, filename=None, width=400, height=300, stroke=3, upsample_mult=5, upsample_x=20):
     width *= upsample_mult
     height *= upsample_mult
-
-    if stroke is None:
-        stroke = upsample_mult
 
     if filename is None:
         filename = 'waveform.png'
 
-    x = range(width)
+    x = range(width*upsample_x)
 
     img = Image.new('RGBA', (width, height), (255, 255, 255, 0))
     draw = ImageDraw.Draw(img)
 
     for channel in range(sound.channels):
-        color = tuple([random.randint(0, 255) for _ in range(3)] + [255])
+        color = tuple([random.randint(0, 200) for _ in range(3)] + [200])
         points = [ sound[i][channel] for i in range(len(sound)) ]
-        points = interpolation.linear(points, width)
+        points = interpolation.linear(points, width*upsample_x)
 
         for pos, point in zip(x, points):
             y = int(((point + 1) / 2) * (height - (stroke * 2)) + stroke)
+            pos /= upsample_x
             draw.ellipse((pos-stroke, y-stroke, pos+stroke, y+stroke), fill=color)
 
     img.thumbnail((width//upsample_mult, height//upsample_mult))
     img.save(filename)
+
