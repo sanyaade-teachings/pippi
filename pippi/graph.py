00531eebd67d88b6a6628a86232a20aecc791416
Erik Schoster <ejschoster@gmail.com>
2017-07-18 21:19:22 -0500

    Eh, make upsampling amount configurable sure why not

--- pippi/graph.py
+++ pippi/graph.py
@@ -2,14 +2,12 @@ import random
 from PIL import Image, ImageDraw
 from . import interpolation
 
-UPSAMPLE_MULT = 10
-
-def waveform(sound, filename=None, width=400, height=200, stroke=None):
-    width *= UPSAMPLE_MULT
-    height *= UPSAMPLE_MULT
+def waveform(sound, filename=None, width=400, height=200, stroke=None, upsample_mult=10):
+    width *= upsample_mult
+    height *= upsample_mult
 
     if stroke is None:
-        stroke = UPSAMPLE_MULT
+        stroke = upsample_mult
 
     if filename is None:
         filename = 'waveform.png'
@@ -28,5 +26,5 @@ def waveform(sound, filename=None, width=400, height=200, stroke=None):
             y = int(((point + 1) / 2) * (height - (stroke * 2)) + stroke)
             draw.ellipse((pos-stroke, y-stroke, pos+stroke, y+stroke), fill=color)
 
-    img.thumbnail((width//UPSAMPLE_MULT, height//UPSAMPLE_MULT))
+    img.thumbnail((width//upsample_mult, height//upsample_mult))
     img.save(filename)
