0a0c6e3ba83e01339d878374e5092906f6e178cd
Erik Schoster <erik@hecanjog.com>
2020-08-12 03:04:17 -0500

    Seed random on multiband spread

--- tests/test_multiband.py
+++ tests/test_multiband.py
@@ -33,6 +33,7 @@ class TestMultiband(TestCase):
         out.write('tests/renders/multiband_customsplit-reconstruct.wav')
 
     def test_spread(self):
+        dsp.seed()
         g = dsp.read('tests/sounds/guitar10s.wav')
         out = multiband.spread(g)
         out.write('tests/renders/multiband_spread-0.5.wav')
