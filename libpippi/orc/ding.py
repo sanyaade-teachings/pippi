0ec798a5a8727b7390e1fc16109bdc0dee4bdca8
Erik Schoster <erik@hecanjog.com>
2022-11-13 16:27:04 -0600

    Simplify astrid test script

--- libpippi/orc/ding.py
+++ libpippi/orc/ding.py
@@ -1,14 +1,9 @@
-from pippi import dsp, fx, oscs
+from pippi import dsp, oscs
 
 LOOP = True
 
 def play(ctx):
-    out = ctx.adc(dsp.rand(1, 3)) 
-    #ctx.log(dsp.rand())
-    #out.write('adc.wav')
-    #out = fx.bpf(out, dsp.rand(100, 10000))
-    #out = fx.hpf(out, 200)
-    out = fx.norm(out, dsp.rand(0.5, 0.6))
-    #out.dub(oscs.SineOsc(freq=dsp.choice([100,200,300,400,500])).play(out.dur) * 0.05)
-    out = out.env('sine')
-    yield out
+    yield oscs.SineOsc(
+            freq=dsp.rand(100, 500),
+            amp=0.1
+        ).play(out.dur).env('pluckout')
