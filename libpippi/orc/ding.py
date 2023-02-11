a071fcc92144ed0b12718d059b3851b552df4e21
Erik Schoster <erik@hecanjog>
2023-02-10 19:23:30 -0600

    Fix ding

--- libpippi/orc/ding.py
+++ libpippi/orc/ding.py
@@ -6,11 +6,8 @@ def play(ctx):
     freqs = tune.degrees([dsp.randint(1,7)], key='c', octave=dsp.randint(3,4))
     stack = ['sine', 'hann', 'tri', 'cos', 'square']
     o = oscs.SineOsc(
-            #stack,
-            #windows=['sine', 'hann'],
             freq=dsp.choice(freqs),
             amp=dsp.rand(0.25, 0.5),
-            pulsewidth=1,
         ).play(dsp.rand(1, 6)).env('rnd')
     o = o.pan(dsp.rand())
 
