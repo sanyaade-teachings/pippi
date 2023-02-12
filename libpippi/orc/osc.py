c7bb30eed450bdabad8b554859584ade3d883499
Erik Schoster <erik@hecanjog.com>
2023-02-12 14:49:14 -0600

    Commit osc script and add some variations

--- libpippi/orc/osc.py
+++ libpippi/orc/osc.py
@@ -1,22 +1,67 @@
-from pippi import dsp, oscs, tune
+from pippi import dsp, oscs, tune, fx, shapes
 
 #LOOP = True
 
 
 def before(ctx):
-    ctx.log('before firing')
+    # The before callback is fired before 
+    # each render -- but only once for each 
+    # group of submix renders when yeilding 
+    # over a polyphonic generator.
+    ctx.log('before render')
 
 def play(ctx):
     length = ctx.cache.get('length', 1)
+
+    # ctx.p contains parameters passed with 
+    # the triggering play command.
+    # Play commands initiated by the MIDI relay 
+    # will include the MIDI note as a parameter.
+    # Parameters are provided as strings.
     note = float(ctx.p.note or 60)
+
+    # ctx.log will write a string to the system log
     ctx.log('note %s' % note)
+
+    # Convert the MIDI note to a frequency
     freq = tune.mtf(note)
 
-    osc = oscs.SineOsc(freq=freq, amp=0.05)
-    ctx.log('length %s' % length)
+    # This sets up a polyphonic generator 
+    # by looping over a yield. Each buffer in 
+    # the generator is rendered and then mixed 
+    # together at the output.
+    for _ in range(dsp.randint(2,3)):
+        # Set up the osc with a randomly detuned freq
+        osc = oscs.SineOsc(freq=freq * dsp.rand(0.99, 1.01), amp=0.15)
 
-    yield osc.play(length).taper(dsp.rand(dsp.MS, 0.1))
+        # Render a buffer
+        out = osc.play(length)
+
+        # Add an envelope
+        out = out.env('pluckout').taper(dsp.MS * 5)
+
+        # Pan to a random position
+        out = out.pan(dsp.rand())
+
+        # Sometimes add foldback distortion
+        if dsp.rand() > 0.5:
+            out = fx.fold(out, dsp.rand(1,2))
+
+        # Sometimes warble the pitch
+        if dsp.rand() > 0.75:
+            out = out.vspeed(shapes.win('sine', 0.5, 1.5))
+
+        # Off to the mixer for playback
+        yield out
 
 def done(ctx):
-    ctx.log('done firing')
-    ctx.cache['length'] = dsp.rand(0.1,1)
+    # The done callback is fired after each 
+    # render completes.
+    ctx.log('done rendering')
+
+    # ctx.cache persists between renders.
+    # Killing the renderer process will 
+    # clear the cache. It is stored as a 
+    # property on the renderer instance, 
+    # not in the redis session.
+    ctx.cache['length'] = dsp.rand(0.3, 3)
